d20a933f3e310eb5185e17447c1d6907aa4e2024
Dede sofian <mdia76248@gmail.com>
2025-01-09 04:56:43 +0700

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pinjam Buku</title>
+    <link rel="stylesheet" href="css/style.css"> <!-- Link to the CSS file -->
+    <link rel="stylesheet" href="css/br.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"><!-- Link to the CSS file -->
+</head>
+<footer>
+    <p>&copy; 2025 SPMN 1 Plered. All rights reserved.</p>
+</footer>
